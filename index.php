a7d7f8d68718ac1f6e78971b71956d688ec6c7b8
Nathan Hosler <92895949+nhosler738@users.noreply.github.com>
2023-02-24 14:26:20 -0400

    added index, css, and assets files

---
+++ index.php
@@ -0,0 +1,64 @@
+
+
+
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AllegroGuitars</title>
+        <link rel="icon" href="/assets/tab.ico">
+        <link rel="stylesheet" href="/style/main.css">
+    </head>
+
+
+<body>
+
+    <div class="navigation-bar">
+        <ul class="bar-1">
+            <!--Homepage img with home link-->
+            <li><a href="/index.php"><img src="/assets/guitar.jpg" alt="Allegro Dafault"></a></li>
+            <li>
+                <form action="" method="get" class="search-form">
+
+
+
+                </form>
+
+            </li>
+
+
+        </ul>
+
+        <ul class="bar-2">
+
+        </ul>
+
+
+
+        </li>
+    </div>
+
+
+</body>
+
+
+
+<footer>
+
+
+
+
+</footer>
+
+</html>
+
+
+
+
+
+
+
+
+
+
